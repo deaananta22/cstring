9794d20e6e198d67fc2001b9fe684ab65bcfb7ef
deaananta22 <112568977+deaananta22@users.noreply.github.com>
2023-04-13 10:42:15 +0700

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int nilai(int a, int b){
+	return a+b;
+}
+int main(){
+//	int arr[2]={1,2};
+//	int *ptr = arr;
+//	
+//	for(int i=0; i<2;i++){
+//		cout<<i<<" "<<*ptr<<endl;
+//		ptr++;
+//	}
+//	
+int a,b;
+cin>>a;
+cin>>b;
+cout<<nilai(a,b);
+
+}
